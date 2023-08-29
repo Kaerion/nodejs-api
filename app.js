1f4c3da9fd9c98f0b40d6f118b5ec51a7ee980c0
Kaerion <jquero89@gmail.com>
2023-08-29 14:54:31 +0200

    Create, Update, Delete endpoints and CORS solution

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
+/* eslint-disable no-debugger */
+const { validateMovie, validatePartialMovie } = require('./schemas/movies.js');
 const express = require('express');
 const movies = require('./movies.json');
+const crypto = require('node:crypto');
+const cors = require('cors');
 
 const app = express();
+app.use(express.json()); // Funcion para traer cuerpo de request con express
 app.disable('x-powered-by');
+
+// Solucion para CORS de metodos complejos (PUT, PATCH, DELETE)
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234'
+    ];
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true);
+    }
+
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    return callback(new Error('Not allowed by CORS'));
+  }
+}));
 
 /** Prueba Hola mundo */
 app.get('/', (req, res) => {
@@ -12,8 +37,30 @@ app.get('/', (req, res) => {
 /** Obtener todas las peliculas o filtrado por genero.
  * Prueba de varios filtrados con req.query
 */
+
 app.get('/movies', (req, res) => {
-  const { genre, year } = req.query;
+  try {
+    const { genre, year } = req.query;
+
+    // Validación de entrada
+    if (genre && typeof genre !== 'string') {
+      return res.status(400).json({ error: 'Invalid genre' });
+    }
+    if (year && isNaN(year)) {
+      return res.status(400).json({ error: 'Invalid year' });
+    }
+
+    const filteredMovies = filterMovies(movies, genre, year);
+
+    res.json(filteredMovies);
+  } catch (error) {
+    // Manejo de errores
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while filtering movies' });
+  }
+});
+
+function filterMovies (movies, genre, year) {
   let filteredMovies = movies;
   if (genre) {
     filteredMovies = filteredMovies.filter(
@@ -23,9 +70,10 @@ app.get('/movies', (req, res) => {
   if (year) {
     filteredMovies = filteredMovies.filter(movie => movie.year.toString() === year.toString());
   }
+  return filteredMovies;
+}
 
-  res.json(filteredMovies);
-});
+/// //////////////////
 
 /** Pelicula por id */
 app.get('/movies/:id', (req, res) => {
@@ -49,6 +97,74 @@ app.get('/movies/genre/:genre', (req, res) => {
   } else {
     res.status(404).json({ message: 'Movies not found with this genre' });
   }
+});
+
+// Crear pelicula
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body);
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  };
+  movies.push(newMovie); // Agregar la nueva película al array en memoria
+
+  // Convertir el array de películas a una cadena en formato JSON
+  /* const jsonMovies = JSON.stringify(movies, null, 2);
+
+  // Escribir la cadena en el archivo movies.json
+  fs.writeFile('./movies.json', jsonMovies, (err) => {
+    if (err) {
+      console.error(err);
+      // Si hubo un error al escribir el archivo, enviar una respuesta con código de estado 500 (Error interno del servidor)
+      return res.status(500).json({ error: 'An error occurred while saving the movie' });
+    }
+
+    // Si no hubo errores, enviar una respuesta con código de estado 201 (Creado)
+    res.status(201).json(newMovie);
+  }); */
+  res.status(201).json(newMovie);
+});
+
+// Actualizar Pelicula
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex(movie => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not Found.' });
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  };
+
+  movies[movieIndex] = updateMovie;
+  return res.json(updateMovie);
+});
+
+// Eliminar Pelicula
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.findIndex(movie => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not Found.' });
+  }
+
+  movies.splice(movieIndex, 1);
+  return res.json({ message: 'Movie Deleted' });
 });
 
 /** Puerto http en escucha. */
---
+++ schemas/movies.js
@@ -0,0 +1,31 @@
+const zod = require('zod');
+
+const movieSchema = zod.object({
+  title: zod.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required'
+  }),
+  year: zod.number().int().min(1900).max(2024),
+  director: zod.string(),
+  duration: zod.number().int().positive(),
+  rate: zod.number().min(0).max(10).default(5),
+  poster: zod.string().url({
+    message: 'Poster must be a valid URL'
+  }),
+  genre: zod.array(
+    zod.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Sci-Fi', 'Thriller'])
+  )
+});
+
+function validateMovie (object) {
+  return movieSchema.safeParse(object);
+}
+
+function validatePartialMovie (object) {
+  return movieSchema.partial().safeParse(object);
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+};
